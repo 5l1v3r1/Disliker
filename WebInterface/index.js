d656a4ab54316e3769b63a92bec8cff5cae47e7d
Yui Hirasawa <bosnianrainbow@gmail.com>
2018-05-11 02:21:47 +0500

    simple form

--- WebInterface/index.js
+++ WebInterface/index.js
@@ -1,14 +1,26 @@
 'use strict';
 
-require('hbs');
+const hbs = require('hbs');
+const hbsutils = require('hbs-utils')(hbs);
 const path = require('path');
 const express = require('express');
 
 const app = express();
+require('./controllers')(app);
 
 const viewsDir = path.join(__dirname, 'views');
+const staticDir = path.join(__dirname, 'static');
+const partialsDir = path.join(viewsDir, 'partials');
 
 app.set('view engine', 'hbs');
 app.set('views', viewsDir);
 
-app.listen(8080);
+app.use(express.static(staticDir));
+
+hbsutils.registerWatchedPartials(partialsDir, () => {
+    app.listen(8080);
+});
+
+/*hbs.registerPartials(partialsDir, () => {
+    app.listen(8080);
+});*/
